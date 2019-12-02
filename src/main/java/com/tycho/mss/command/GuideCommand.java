060ddaf04422aec47411e8e1cbfb8bbed9a95e44
Tycho <tycho.bellers999@gmail.com>
2019-12-01 20:36:57 -0800

    Improved "guide" command.

--- src/main/java/com/tycho/mss/command/GuideCommand.java
+++ src/main/java/com/tycho/mss/command/GuideCommand.java
@@ -2,8 +2,11 @@ package com.tycho.mss.command;
 
 import com.tycho.mss.Player;
 import com.tycho.mss.ServerShell;
+import easytasks.Task;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,54 +14,56 @@ public class GuideCommand extends Command {
 
     private static final Pattern POSITION_PATTERN = Pattern.compile("^\\[\\d{2}:\\d{2}:\\d{2}] \\[Server thread\\/INFO]: (?<player>.+) has the following entity data: \\[(?<x>-?\\d+)\\.\\d+d, (?<y>-?\\d+)\\.\\d+d, (?<z>-?\\d+)\\.\\d+d]");
 
-    private Thread thread;
+    private final Map<String, GuideTask> tasks = new HashMap<>();
 
     @Override
     public void execute(String player, ServerShell serverShell, String... parameters) throws Exception {
-        if (parameters.length > 0){
-            if ("stop".equals(parameters[0])){
-                if (this.thread != null) thread.interrupt();
-                this.thread = null;
-                return;
-            }
+        if (parameters.length < 1){
+            throw new InvalidParametersException();
         }
-        //Get player position
-        Matcher matcher = serverShell.awaitResult("data get entity " + player + " Pos", POSITION_PATTERN);
-        final int x = Integer.parseInt(matcher.group("x"));
-        final int y = Integer.parseInt(matcher.group("y"));
-        final int z = Integer.parseInt(matcher.group("z"));
 
-        //Determine destination
-        /*final Player destination = serverShell.getPlayer(parameters[0]);
-        matcher = serverShell.awaitResult("data get entity " + destination + " Pos", POSITION_PATTERN);
-        final int dx = Integer.parseInt(matcher.group("x"));
-        final int dy = Integer.parseInt(matcher.group("y"));
-        final int dz = Integer.parseInt(matcher.group("z"));*/
+        //Stop
+        if ("stop".equals(parameters[0])){
+            if (this.tasks.get(player) != null) this.tasks.get(player).stop();
+            this.tasks.put(player, null);
+            return;
+        }
 
-        final int dx = 0;
-        final int dy = 0;
-        final int dz = 0;
+        //If there is only 1 parameter it might be a player or the index of a saved location
+        if (parameters.length == 1){
+            try {
+                final int index = Integer.parseInt(parameters[0]);
+                final SavedLocation savedLocation = serverShell.getPlayer(player).getSavedLocations().get(index);
+                final int dx = savedLocation.getX();
+                final int dy = savedLocation.getY();
+                final int dz = savedLocation.getZ();
 
-        final double direction = Math.atan2(dz - z, dx - x);
-        if (thread != null) thread.interrupt();
-        thread = new Thread(() -> {
-            while (true){
-                for (int i = 1; i < 10; i++){
-                    try {
-                        serverShell.execute("execute at TychoTheTaco run particle composter ~" + (i * Math.cos(direction)) + " ~" + 0 + " ~" + (i * Math.sin(direction)) + " 0 0 0 0 1 normal");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                try {
-                    Thread.sleep(100);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
+                if (tasks.get(player) != null) tasks.get(player).stop();
+                final GuideTask guideTask = new GuideTask(player, dx + " " + dy + " " + dz, serverShell);
+                this.tasks.put(player, guideTask);
+                guideTask.startOnNewThread();
+            }catch (NumberFormatException e){
+                if (tasks.get(player) != null) tasks.get(player).stop();
+                final GuideTask guideTask = new GuideTask(player, parameters[0], serverShell);
+                this.tasks.put(player, guideTask);
+                guideTask.startOnNewThread();
             }
-        });
-        thread.start();
+        }else if (parameters.length == 3){
+            try {
+                final int dx = Integer.parseInt(parameters[0]);
+                final int dy = Integer.parseInt(parameters[1]);
+                final int dz = Integer.parseInt(parameters[2]);
+
+                if (tasks.get(player) != null) tasks.get(player).stop();
+                final GuideTask guideTask = new GuideTask(player, dx + " " + dy + " " + dz, serverShell);
+                this.tasks.put(player, guideTask);
+                guideTask.startOnNewThread();
+            }catch (NumberFormatException e){
+                throw new InvalidParametersException();
+            }
+        }else{
+            throw new InvalidParametersException();
+        }
     }
 
     @Override
@@ -74,5 +79,66 @@ public class GuideCommand extends Command {
     @Override
     public String getDescription() {
         return "Guides you to another player or location";
+    }
+
+    private static class GuideTask extends Task {
+
+        private final String player;
+
+        private final String target;
+
+        private final ServerShell serverShell;
+
+        public GuideTask(String player, String target, ServerShell serverShell) {
+            this.player = player;
+            this.target = target;
+            this.serverShell = serverShell;
+        }
+
+        @Override
+        protected void run() {
+            while (isRunning()){
+                try {
+                    //Get player position
+                    Matcher matcher = serverShell.awaitResult("data get entity " + player + " Pos", POSITION_PATTERN);
+                    final int x = Integer.parseInt(matcher.group("x"));
+                    final int y = Integer.parseInt(matcher.group("y"));
+                    final int z = Integer.parseInt(matcher.group("z"));
+
+                    //Determine destination
+                    final int dx;
+                    final int dy;
+                    final int dz;
+                    final String[] split = target.split(" ");
+                    if (split.length == 3){
+                        dx = Integer.parseInt(split[0]);
+                        dy = Integer.parseInt(split[1]);
+                        dz = Integer.parseInt(split[2]);
+                    }else{
+                        //Get player position
+                        matcher = serverShell.awaitResult("data get entity " + target + " Pos", POSITION_PATTERN);
+                        dx = Integer.parseInt(matcher.group("x"));
+                        dy = Integer.parseInt(matcher.group("y"));
+                        dz = Integer.parseInt(matcher.group("z"));
+                    }
+
+                    //Calculate direction
+                    final double direction = Math.atan2(dz - z, dx - x);
+
+                    //Spawn particles
+                    for (int i = 1; i < 10; i++){
+                        try {
+                            serverShell.execute("execute at " + player + " run particle composter ~" + (i * Math.cos(direction)) + " ~" + 0 + " ~" + (i * Math.sin(direction)) + " 0 0 0 0 1 normal");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Thread.sleep(100);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
